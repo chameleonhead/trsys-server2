8d989a3afc8e3d00ec0b318e5b93040b72794a99
Yuto Nagano <chamelion@hotmail.co.jp>
2021-09-01 23:05:47 +0900

    実際のログを元にテストケースを作成

--- tests/Trsys.CopyTrading.Tests/EaLogParserTest.cs
+++ tests/Trsys.CopyTrading.Tests/EaLogParserTest.cs
@@ -18,7 +18,44 @@ namespace Trsys.CopyTrading.Tests
                     "Publisher",
                     "TOKEN",
                     "VERSION",
-                    "1629456515:DEBUG:Init\r\n1629456515:DEBUG:Local order opened. LocalOrder = 0/27636811/GBPJPY.oj1m/1\r\n1629456688:DEBUG:Local order closed. LocalOrder = 0/27636811/GBPJPY.oj1m/1\r\n1629456952:DEBUG:Deinit. Reason = 3")
+                    string.Join("\r\n", new[] {
+                        "1629456515:DEBUG:Init",
+                        "1629456515:DEBUG:Local order opened. LocalOrder = 0/27636811/GBPJPY.oj1m/1",
+                        "1629456688:DEBUG:Local order closed. LocalOrder = 0/27636811/GBPJPY.oj1m/1",
+                        "1629456952:DEBUG:Deinit. Reason = 3"
+                    }))
+                .ToArray();
+            Assert.AreEqual(4, events.Count());
+            Assert.AreEqual("EaLogInit", events[0].Type);
+            Assert.AreEqual("EaLogLocalOrderOpened", events[1].Type);
+            Assert.AreEqual("EaLogLocalOrderClosed", events[2].Type);
+            Assert.AreEqual("EaLogDeinit", events[3].Type);
+        }
+        [TestMethod]
+        public void SubscriberTypicalLog_ShouldParsed()
+        {
+            var events = EaLogParser
+                .Parse(
+                    DateTimeOffset.Parse("2021-08-23T13:33:23.927Z"),
+                    "KEY",
+                    "Subscriber",
+                    "TOKEN",
+                    "VERSION",
+                    string.Join("\r\n", new[] {
+                        "1629381997:DEBUG:Init",
+                        "1629456138:DEBUG:Server order opened. ServerOrder = 27636811/GBPJPY/1",
+                        "1629456138:DEBUG:CalculateVolume: Symbol = GBPJPY.oj1m, Margin for a lot = 746305.00000000, Step = 0.01000000",
+                        "1629456138:DEBUG:CalculateVolume: Free margin = 1965498.00000000, Leverage = 25, Percentage = 98.00000000, Calculated volume = 2.58000000",
+                        "1629456138:DEBUG:OrderSend executing: ServerOrder = 27636811/GBPJPY/1, Calculated lots = 2.58000000", "1629456139:INFO:OrderSend succeeded: 27636811, OrderTicket = 27636816",
+                        "1629456139:DEBUG:Local order opened. LocalOrder = 27636811/27636816/GBPJPY.oj1m/1",
+                        "1629456139:DEBUG:OPEN:27636811:GBPJPY.oj1m:1:27636816:27636816:GBPJPY.oj1m:1:149.24900000:2.58000000:1629456139",
+                        "1629456688:DEBUG:Server order closed. ServerOrder = 27636811/GBPJPY/1",
+                        "1629456688:DEBUG:OrderClose executing: 27636811, OrderTicket = 27636816",
+                        "1629456688:INFO:OrderClose succeeded: 27636811, OrderTicket = 27636816",
+                        "1629456688:DEBUG:Local order closed. LocalOrder = 27636811/27636816/GBPJPY.oj1m/1",
+                        "1629456688:DEBUG:CLOSE:27636811:GBPJPY.oj1m:1:27636816:27636816:GBPJPY.oj1m:1:149.36700000:2.58000000:1629456689:-30444.00000000",
+                        "1629704001:DEBUG:Deinit. Reason = 9"
+                    }))
                 .ToArray();
             Assert.AreEqual(4, events.Count());
             Assert.AreEqual("EaLogInit", events[0].Type);
