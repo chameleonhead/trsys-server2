a5f5e42d03929fcc55f9d7216a083ce8da388c2a
Yuto Nagano <chamelion@hotmail.co.jp>
2021-09-04 23:23:22 +0900

    LoadTestingにコピートレードサービスのサーバークラスを作成

---
+++ tests/LoadTesting/Server/CopyTrading/CopyTradingServer.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Reflection;
+using Trsys.CopyTrading.Service;
+
+namespace LoadTesting.Server.CopyTrading
+{
+    public class CopyTradingServer : IDisposable
+    {
+        private readonly TestServer testServer;
+
+        public CopyTradingServer()
+        {
+            var path = Assembly.GetAssembly(typeof(CopyTradingServer))
+              .Location;
+
+            var hostBuilder = new WebHostBuilder()
+                .UseContentRoot(Path.GetDirectoryName(path))
+                .ConfigureAppConfiguration(cb =>
+                {
+                    cb.AddJsonFile("Server/CopyTrading/appsettings.json");
+                }).UseStartup<Startup>();
+
+            testServer = new TestServer(hostBuilder);
+        }
+
+        public HttpClient CreateClient()
+        {
+            return testServer.CreateClient();
+        }
+
+        public void Dispose()
+        {
+            testServer.Dispose();
+            GC.SuppressFinalize(this);
+        }
+        public static CopyTradingServer CreateServer()
+        {
+            return new CopyTradingServer();
+        }
+    }
+}
