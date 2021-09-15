913391db02a5730c969dd894e19bc3c53d638bb9
Yuto Nagano <chamelion@hotmail.co.jp>
2021-09-15 13:12:37 +0900

    web と copytrading-service に OpenTelemetry を設定

--- src/Trsys.CopyTrading.Service/Startup.cs
+++ src/Trsys.CopyTrading.Service/Startup.cs
@@ -4,8 +4,10 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using OpenTelemetry;
+using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 using System;
+using System.Collections.Generic;
 using Trsys.CopyTrading.Application;
 using Trsys.CopyTrading.Infrastructure;
 using Trsys.Events;
@@ -31,7 +33,10 @@ namespace Trsys.CopyTrading.Service
         {
             var tracerProvider = Sdk.CreateTracerProviderBuilder()
                 .SetSampler(new AlwaysOnSampler())
-                .AddSource("MySource")
+                .AddSource("Trsys.CopyTrading.Service")
+                .SetResourceBuilder(ResourceBuilder
+                    .CreateDefault()
+                    .AddService(Environment.GetEnvironmentVariable("WEBSITE_SITE_NAME") ?? "copy-trading-server"))
                 .AddZipkinExporter(options =>
                 {
                     options.Endpoint = new Uri("http://localhost:9411/api/v2/spans");
@@ -43,11 +48,29 @@ namespace Trsys.CopyTrading.Service
             }
             app.Use(async (context, next) =>
             {
-                var tracer = tracerProvider.GetTracer("MySource");
+                var tracer = tracerProvider.GetTracer("Trsys.CopyTrading.Service");
                 using var span = tracer.StartSpan(context.Request.Path, SpanKind.Server);
                 try
                 {
+                    span.SetAttribute("service.name", Environment.GetEnvironmentVariable("WEBSITE_SITE_NAME") ?? "copy-trading-server");
+                    span.SetAttribute("service.namespace", "Trsys.CopyTrading.Service");
+                    span.SetAttribute("service.instance.id", Environment.GetEnvironmentVariable("WEBSITE_INSTANCE_ID") ?? "Unknown");
+                    span.SetAttribute("service.version", "20210915");
+                    span.SetAttribute("http.host", context.Request.Host.Value);
+                    span.SetAttribute("http.method", context.Request.Method);
+                    var builder = new UriBuilder();
+                    builder.Scheme = context.Request.Scheme;
+                    builder.Host = context.Request.Host.Host;
+                    if (context.Request.Host.Port.HasValue)
+                    {
+                        builder.Port = context.Request.Host.Port.Value;
+                    }
+                    builder.Path = context.Request.Path;
+                    builder.Query = context.Request.QueryString.ToString();
+                    span.SetAttribute("http.url", builder.Uri.ToString());
                     await next();
+                    span.SetAttribute("http.status_code", context.Response.StatusCode);
+
                 }
                 catch (Exception ex)
                 {
--- src/Trsys.Frontend.Web/Startup.cs
+++ src/Trsys.Frontend.Web/Startup.cs
@@ -3,6 +3,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using System;
+using System.Collections.Generic;
 using Trsys.Backoffice;
 using Trsys.CopyTrading;
 using Trsys.CopyTrading.Application;
@@ -46,6 +51,17 @@ namespace Trsys.Frontend.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            var tracerProvider = Sdk.CreateTracerProviderBuilder()
+                .SetSampler(new AlwaysOnSampler())
+                .AddSource("Trsys.Frontend.Web")
+                .SetResourceBuilder(ResourceBuilder
+                    .CreateDefault()
+                    .AddService(Environment.GetEnvironmentVariable("WEBSITE_SITE_NAME") ?? "trsys-server"))
+                .AddZipkinExporter(options =>
+                {
+                    options.Endpoint = new Uri("http://localhost:9411/api/v2/spans");
+                })
+                .Build();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -56,6 +72,36 @@ namespace Trsys.Frontend.Web
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.Use(async (context, next) =>
+            {
+                var tracer = tracerProvider.GetTracer("Trsys.Frontend.Web");
+                using var span = tracer.StartSpan(context.Request.Path, SpanKind.Server);
+                try
+                {
+                    span.SetAttribute("service.name", Environment.GetEnvironmentVariable("WEBSITE_SITE_NAME") ?? "trsys-server");
+                    span.SetAttribute("service.namespace", "Trsys.Frontend.Web");
+                    span.SetAttribute("service.instance.id", Environment.GetEnvironmentVariable("WEBSITE_INSTANCE_ID") ?? "Unknown");
+                    span.SetAttribute("service.version", "20210915");
+                    span.SetAttribute("http.host", context.Request.Host.Value);
+                    span.SetAttribute("http.method", context.Request.Method);
+                    var builder = new UriBuilder();
+                    builder.Scheme = context.Request.Scheme;
+                    builder.Host = context.Request.Host.Host;
+                    if (context.Request.Host.Port.HasValue)
+                    {
+                        builder.Port = context.Request.Host.Port.Value;
+                    }
+                    builder.Path = context.Request.Path;
+                    builder.Query = context.Request.QueryString.ToString();
+                    span.SetAttribute("http.url", builder.Uri.ToString());
+                    await next();
+                    span.SetAttribute("http.status_code", context.Response.StatusCode);
+                }
+                catch (Exception ex)
+                {
+                    span.RecordException(ex);
+                }
+            });
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseRouting();
