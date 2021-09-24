51ee41d43c5c2de9c1d6634d20158eee5f7d70da
Yuto Nagano <chamelion@hotmail.co.jp>
2021-09-24 12:09:01 +0900

    管理用ダッシュボードのモック作成

---
+++ src/Trsys.Frontend.Web/Controllers/AdminController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Trsys.Frontend.Web.Controllers
+{
+    [Route("admin")]
+    public class AdminController : Controller
+    {
+        [HttpGet("")]
+        public ActionResult Index()
+        {
+            return RedirectToAction(nameof(Dashboard));
+        }
+
+        [HttpGet("dashboard")]
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
